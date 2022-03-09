3a242f453d973776708420fff80b0a70f6a74f66
julimone <julianacora2708@gmail.com>
2022-03-09 15:21:49 -0500

    Adding remainig stub routes

--- controllers/places.js
+++ controllers/places.js
@@ -1,4 +1,4 @@
-const router = ('express.').Router()
+const router = require('express').Router()
 
 router.get('/', (req, res) => {
     res.send('GET/places')
--- index.js
+++ index.js
@@ -2,13 +2,15 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+app.use('/places', require('./controllers/places'))
+
 app.get('/', function (req, res) {
     res.send('Hello World')
 })
 
 
 app.get('*', (req, res) => {
-    res.statuss(404).send('<h1> 404 Page </h1>')
+    res.status(404).send('<h1> 404 Page </h1>')
 })
 
 app.listen(process.env.PORT)
