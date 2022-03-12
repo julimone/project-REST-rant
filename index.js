718542f3e8a6c6e6a68ae7d8e88f62f5005ea49f
julimone <julianacora2708@gmail.com>
2022-03-12 12:17:12 -0500

    fixed issues in the controller

--- index.js
+++ index.js
@@ -2,15 +2,17 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
 app.use('/places', require('./controllers/places'))
 
-app.get('/', function (req, res) {
-    res.send('Hello World')
+app.get('/', (req, res) => {
+    res.render('home')
 })
 
-
 app.get('*', (req, res) => {
-    res.status(404).send('<h1> 404 Page </h1>')
+    res.render('error404')
 })
 
 app.listen(process.env.PORT)
