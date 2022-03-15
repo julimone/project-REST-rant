0769c2c2958d19e1fa523eaa31c01dc436139355
julimone <julianacora2708@gmail.com>
2022-03-14 19:06:44 -0500

    Adding and rendering a new view

---
+++ views/places/new.jsx
@@ -0,0 +1,20 @@
+const React = require('react')
+const Def = require('../default')
+
+function new_form() {
+    return (
+        <Def>
+            <main>
+                <h1>Add a New Place</h1>
+                <form method='POST' action='/places'>
+                    <div>
+                        <label htmlFor='name'> Place Name</label>
+                        <input id="name" name='name' />
+                    </div>
+                </form>
+            </main>
+        </Def>
+    )
+}
+
+module.exports = new_form
