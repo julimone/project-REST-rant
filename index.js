e37ba418128304a835017295c71b94f5f0c7e38c
julimone <julianacora2708@gmail.com>
2022-03-14 19:53:42 -0500

    Form data is getting through

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
 
 //Controllers $ Routes
+app.use(express.urlencoded({ extended: true }))
 app.use('/places', require('./controllers/places'))
+
 
 app.get('/', (req, res) => {
     res.render('home')
