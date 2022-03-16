1bd91d9d30911528916109611abf849c10cf2d63
julimone <julianacora2708@gmail.com>
2022-03-16 10:30:42 -0500

    Added the edit functionality

--- controllers/places.js
+++ controllers/places.js
@@ -8,7 +8,6 @@ router.get('/', (req, res) => {
 
 // More code ...
 
-
 router.get('/', (req, res) => {
     res.render('places/index', { places })
 })
@@ -45,6 +44,7 @@ router.post('/', (req, res) => {
     places.push(req.body)
     res.redirect('/places')
 })
+
 router.delete('/places/:id', (req, res) => {
     let id = Number(req.params.id)
     if (isNaN(id)) {
@@ -57,6 +57,32 @@ router.delete('/places/:id', (req, res) => {
         places.splice(id, 1)
         res.send('/places')
     }
-})
 
-module.exports = router
+    router.put('/:id', (req, res) => {
+        let id = Number(req.params.id)
+        if (isNaN(id)) {
+            res.render('error404')
+        }
+        else if (!places[id]) {
+            res.render('error404')
+        }
+        else {
+            // Dig into req.body and make sure data is valid
+            if (!req.body.pic) {
+                // Default image if one is not provided
+                req.body.pic = 'http://placekitten.com/400/400'
+            }
+            if (!req.body.city) {
+                req.body.city = 'Anytown'
+            }
+            if (!req.body.state) {
+                req.body.state = 'USA'
+            }
+
+            // Save the new data into places[id]
+            places[id] = req.body
+            res.redirect(`/places/${id}`)
+        }
+    })
+
+    module.exports = router
